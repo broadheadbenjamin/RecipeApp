98e142143fc8a6ba89dbee67f7a5b2a2a4acba5a
Ben Broadhead <broadhead.benjamin@gmail.com>
2018-07-11 16:09:12 -0600

    Created a fragment class and had it work in a recycler view.

--- app/src/main/java/com/ge/recipeapp/MainActivity.java
+++ app/src/main/java/com/ge/recipeapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.ge.recipeapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListFragment fragment = new ListFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.placeHolder, fragment);
+        fragmentTransaction.commit();
     }
 }
