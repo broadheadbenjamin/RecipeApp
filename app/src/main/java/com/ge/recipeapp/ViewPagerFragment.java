66417bbb6617ba70e91a1201da206bf5ccdef63f
Ben Broadhead <broadhead.benjamin@gmail.com>
2018-07-12 09:49:45 -0600

    Added fragments for the ingredients and directions views.

--- app/src/main/java/com/ge/recipeapp/ViewPagerFragment.java
+++ app/src/main/java/com/ge/recipeapp/ViewPagerFragment.java
@@ -4,15 +4,45 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class ViewPagerFragment extends Fragment {
+    public static final String KEY_RECIPE_INDEX = "recipe_index";
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        int index = getArguments().getInt("recipe_index");
+        getActivity().setTitle(Recipes.names[index]);
         View view = inflater.inflate(R.layout.fragment_viewpager, container, false);
+
+        final IngredientsFragment ingredientsFragment = new IngredientsFragment();
+        final DirectionsFragment directionsFragment = new DirectionsFragment();
+
+        ViewPager viewPager = view.findViewById(R.id.viewPager);
+        viewPager.setAdapter(new FragmentPagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return position == 0 ? ingredientsFragment : directionsFragment;
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        });
+
         return view;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getActivity().setTitle(getResources().getString(R.string.app_name));
     }
 }
